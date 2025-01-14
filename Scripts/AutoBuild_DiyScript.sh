8d3097e5d42e46b94bcf9d6523c50f595b631e76
webappstars <141623219+webappstars@users.noreply.github.com>
2025-01-14 08:15:56 -0600

    Update AutoBuild_DiyScript.sh

--- Scripts/AutoBuild_DiyScript.sh
+++ Scripts/AutoBuild_DiyScript.sh
@@ -100,18 +100,22 @@ EOF
 		git reset --hard 1627fd2c745e496134834a8fb8145ba0aa458ae9
 		
 		rm -r ${FEEDS_LUCI}/luci-theme-argon*
-		AddPackage other vernesong OpenClash dev
-		AddPackage other jerrykuku luci-app-argon-config 18.06
-		AddPackage other sbwml luci-app-mosdns v5-lua
-		AddPackage themes jerrykuku luci-theme-argon 18.06
-		AddPackage themes thinktip luci-theme-neobird main
-		AddPackage msd_lite ximiTech luci-app-msd_lite main
-		AddPackage msd_lite ximiTech msd_lite main
-		AddPackage iptvhelper riverscn openwrt-iptvhelper master
+		#AddPackage other vernesong OpenClash dev
+		#AddPackage other jerrykuku luci-app-argon-config 18.06
+		#AddPackage other sbwml luci-app-mosdns v5-lua
+		#AddPackage themes jerrykuku luci-theme-argon 18.06
+		#AddPackage themes thinktip luci-theme-neobird main
+		#AddPackage msd_lite ximiTech luci-app-msd_lite main
+		#AddPackage msd_lite ximiTech msd_lite main
+		#AddPackage iptvhelper riverscn openwrt-iptvhelper master
 		rm -r ${FEEDS_PKG}/mosdns
 		rm -r ${FEEDS_LUCI}/luci-app-mosdns
+		rm -r ${FEEDS_PKG}/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
+                rm -r ${FEEDS_PKG}/utils/v2dat
+                rm -r ${FEEDS_PKG}/lang/golang
 		rm -r ${FEEDS_PKG}/curl
 		rm -r ${FEEDS_PKG}/msd_lite
+                git clone https://github.com/kenzok8/golang ${FEEDS_PKG}/lang/golang
 		Copy ${CustomFiles}/curl ${FEEDS_PKG}
 		
 		case "${TARGET_BOARD}" in
