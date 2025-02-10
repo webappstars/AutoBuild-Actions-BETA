9f05a147e3ef973553c4e9c1280ad782553d4e1a
webappstars <141623219+webappstars@users.noreply.github.com>
2025-02-10 07:58:30 -0600

    Update AutoBuild_DiyScript.sh

--- Scripts/AutoBuild_DiyScript.sh
+++ Scripts/AutoBuild_DiyScript.sh
@@ -192,7 +192,7 @@ Firmware_Diy() {
 		        xray_version="25.1.30"
                         wget --quiet --no-check-certificate -P /tmp \
                                https://github.com/XTLS/Xray-core/releases/download/v{xray_version}/Xray-linux-mips32le.zip
-  
+                        unzip /tmp/Xray-linux-mips32le.zip xray_softfloat -d /tmp
                ;;
 		esac
 	;;
