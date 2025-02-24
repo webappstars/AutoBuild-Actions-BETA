82690459e07f4118591d9ee87ecb302ff9363d9c
webappstars <141623219+webappstars@users.noreply.github.com>
2025-02-23 18:20:05 -0600

    Update AutoBuild_DiyScript.sh

--- Scripts/AutoBuild_DiyScript.sh
+++ Scripts/AutoBuild_DiyScript.sh
@@ -163,7 +163,7 @@ Firmware_Diy() {
 		;;
                 hiwifi_hc5962)
                         rm -r feeds/luci/applications/luci-app-passwall			
-                        find ${WORK}/package/ | grep Makefile | grep xray | xargs rm -f
+                        find ${WORK}/package/ | grep Makefile | grep xray-core | xargs rm -f
 			find ${WORK}/package/ | grep Makefile | grep sing-box | xargs rm -f
                         AddPackage passwall xiaorouji openwrt-passwall-packages main
 			AddPackage passwall-luci xiaorouji openwrt-passwall main
