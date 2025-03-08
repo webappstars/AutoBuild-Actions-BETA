8e9b89ff22f4eb61bff673d196cd9e3be38e2086
webappstars <141623219+webappstars@users.noreply.github.com>
2025-03-07 20:10:06 -0500

    Update AutoBuild_DiyScript.sh

--- Scripts/AutoBuild_DiyScript.sh
+++ Scripts/AutoBuild_DiyScript.sh
@@ -116,8 +116,8 @@ Firmware_Diy() {
                 #rm -r feeds/other/lean/luci-app-turboacc
 		#rm -rf feeds/packages/net/{xray*,v2ray*,smartdns}
                 rm -rf feeds/packages/utils/v2dat
-                rm -rf feeds/packages/lang/golang
-                git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+                #rm -rf feeds/packages/lang/golang
+                #git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 		#rm -r ${FEEDS_LUCI}/luci-app-argon-config
 		
 		AddPackage other jerrykuku luci-app-argon-config master 
